7f8bf561554ef0d745c654f1bc8f278a827cc803
caowei <solocao@126.com>
2018-12-05 16:00:37 +0800

    汽车分类

--- src/components/car/BrandCar.vue
+++ src/components/car/BrandCar.vue
@@ -1,41 +1,27 @@
 <template>
-  <div class="car-category">
-    <div class="c-sider">
-      <a href="">
-        <img class="c-logo" :src="list.logo" alt="">
-      </a>
-      <div class="c-name">
-        <a href="">{{list.name}}</a>
-      </div>
-    </div>
-    <div class="c-content">
-      <div class="c-operater">
+  <div class="brand-car">
+
+    <!-- <div class="c-operater">
         <Poptip title="新增子品牌">
           <span>
             点击新增子品牌
           </span>
-          <!-- 删除 -->
           <div slot="content">
             <Input size="small" v-model="name" placeholder="请输入子品牌名称" style="width: 120px" />
             <Button type="primary" size="small" class="ml10" @click="addSubBrand()">确定</Button>
           </div>
         </Poptip>
-      </div>
+      </div> -->
 
-      <div v-for="brand in list.brand" :key="brand._id">
-        <div class="c-title">
-          <span> {{brand.name}}</span>
-          <span>
-            新增子品牌
-          </span>
-        </div>
-        <div class="items-wraper">
-          <car-edit-item :sub_brand_id="brand._id"></car-edit-item>
-          <car-item v-for="car in brand.car" :key="car._id" :name="car.name" :img="car.img" :car_id="car._id"></car-item>
-        </div>
-      </div>
+    <div class="brand-title">
+      <span> {{brand.name}}</span>
+    </div>
+    <div class="items-wraper">
+      <car-edit-item :sub_brand_id="brand._id"></car-edit-item>
+      <car-item v-for="car in brand.car" :key="car._id" :name="car.name" :img="car.img" :car_id="car._id"></car-item>
     </div>
   </div>
+
 </template>
 <script>
 import CarItem from '_c/car/CarItem.vue'
@@ -46,19 +32,20 @@ export default {
     CarEditItem
   },
   props: {
-    list: {
+    // 品牌
+    brand: {
       type: Object,
       default: null
     }
   },
-  data() {
+  data () {
     return {
       name: null
     }
   },
   methods: {
     // 新增子品牌
-    async  addSubBrand() {
+    async  addSubBrand () {
       const params = {
         url: 'car/brand/sub',
         payload: {
@@ -80,44 +67,17 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  .car-category {
+  .brand-car {
     position: relative;
     width: 100%;
-    min-height: 88px;
     border-bottom: 1px solid #e1e3e8;
 
-    .c-sider {
-      width: 88px;
-      float: left;
-      text-align: center;
-      padding-top: 10px;
-      background-color: #fafbfc;
-      .c-logo {
-        width: 50px;
-        height: 50px;
-      }
-      .c-name {
-        a {
-          font-size: 14px;
-          color: #333;
-          margin-bottom: -5px;
-        }
-      }
-    }
-  }
-
-  .c-content {
-    margin-left: 88px;
-    // border-left: 1px solid #e7eaec;
-    .c-title {
+    .brand-title {
       font-size: 14px;
       font-weight: bold;
       color: #3b5998;
-      margin-left: 20px;
+      margin-left: 10px;
       padding-top: 10px;
-      display: flex;
-      justify-content: space-around;
-      align-items: center;
     }
     .items-wraper {
       display: flex;
--- src/view/car/category.vue
+++ src/view/car/category.vue
@@ -6,12 +6,34 @@
         <Button type="primary" size="small" @click="userList">新增品牌</Button>
       </div>
       <div class="car-wraper">
+        <!-- 左侧分类 -->
         <div class="c-category">
-          <div v-for="i in 2">ad</div>
+          <div v-for="(category,index) in categoryList" :key="index">
+            <div class="c-cate">
+              {{category.initial}}
+            </div>
+            <div class="c-brand" v-for="(cate,index) in category.list" :key="index" @click="switchCategory(cate)">
+              <img :src="cate.logo" alt="">
+              <span> {{cate.name}}</span>
+            </div>
+          </div>
         </div>
+        <!-- 右侧详情 -->
         <div class="c-detail">
-          asff
-
+          <div class="c-header">
+            <span class="c-name"> {{category.name}}</span>
+            <Poptip placement="left">
+              <span class="c-ope">
+                新增子品牌
+              </span>
+              <!-- 删除 -->
+              <div slot="content">
+                <Input size="small" v-model="name" placeholder="请输入子品牌名称" style="width: 120px" />
+                <Button type="primary" size="small" class="ml10" @click="addSubBrand()">确定</Button>
+              </div>
+            </Poptip>
+          </div>
+          <brand-car :brand="brand" v-for="(brand,index) in brandList" :key="index"></brand-car>
         </div>
       </div>
     </Card>
@@ -19,16 +41,23 @@
   </div>
 </template>
 <script>
-import CarCategory from '_c/car/CarCategory'
 import CarBrandAdd from '_c/modal/CarBrandAdd'
+import BrandCar from '@components/car/BrandCar'
 export default {
   components: {
-    CarCategory,
+    BrandCar,
     CarBrandAdd
   },
   data () {
     return {
-      carlist: []
+      carlist: [],
+      categoryList: [],
+      // 品牌列表
+      brandList: [],
+      category: {
+        name: null
+      },
+      name: null
 
     }
   },
@@ -36,17 +65,53 @@ export default {
     // 获取汽车列表
     async getCarList () {
       const params = {
-        url: '/car/category/qwe',
+        url: '/car/category/list',
         payload: {},
         auth: true
       }
       const result = await this.get(params)
 
       if (result.code === 1) {
-        this.carlist = result.data
+        this.categoryList = result.data
+        console.log('看看车辆')
+        console.log(this.carlist)
+      }
+    },
+    // 切换汽车分类
+    switchCategory (category) {
+      this.category = category
+      this.getCarBybrand(category._id)
+    },
+    //
+    async getCarBybrand (brandId) {
+      const params = {
+        url: `car/${brandId}`,
+        payload: {},
+        auth: true
+      }
+      const result = await this.get(params)
+      console.log('看看结果')
+      console.log(result)
+      if (result.code === 1) {
+        this.brandList = result.data
+      }
+    },
+    // 新增子品牌
+    async  addSubBrand () {
+      const params = {
+        url: 'car/brand/sub',
+        payload: {
+          list_id: this.category._id,
+          name: this.name
+        },
+        auth: true
+      }
+      const result = await this.post(params)
+      if (result.code === 1) {
+        this.$Message.info(result.msg)
+        this.brandList = result.data.brand
       }
     }
-
   },
   created () {
     this.getCarList()
@@ -62,7 +127,6 @@ export default {
     .car-card {
       height: 100%;
       .car-wraper {
-        background: red;
         position: absolute;
         top: 51px;
         bottom: 0px;
@@ -70,18 +134,60 @@ export default {
         width: 100%;
         flex: 1;
         display: flex;
+        overflow: scroll;
 
         .c-category {
           width: 200px;
           height: 100%;
-          background: yellow;
+          overflow: scroll;
+
+          .c-cate {
+            height: 30px;
+            background: #f2f2f2;
+            display: flex;
+            align-items: center;
+            padding-left: 10px;
+          }
+
+          .c-brand {
+            background: white;
+            height: 40px;
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+            img {
+              width: 40px;
+              height: 40px;
+            }
+          }
         }
         .c-detail {
           height: 100%;
+          overflow: scroll;
           flex: 1;
-          background: green;
+          .c-header {
+            padding-left: 10px;
+            padding-right: 10px;
+            height: 30px;
+            background: #f2f2f2;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            .c-name {
+              color: #0254a1;
+            }
+            .c-ope {
+              cursor: pointer;
+              font-size: 12px;
+            }
+          }
         }
       }
     }
+  }
+
+  .ml10 {
+    margin-left: 10px;
   }
 </style>
